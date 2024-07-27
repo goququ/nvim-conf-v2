81059868a5ecc7d51f9e6e863d179d1e92ba7cf2
Ruslan Nasyrov <ruslan.nasyrov.f@gmail.com>
2024-07-27 22:36:05 +0500

    added spell

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -29,9 +27,12 @@ return {
       opt = { -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
+        spell = true, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        spelllang = "ru,en",
         wrap = false, -- sets vim.opt.wrap
+        list = true,
+        listchars = "tab:> ,trail:-,eol:$",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
