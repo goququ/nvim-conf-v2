dd8acada8bd4e7dba46512de093bc880bfb0ff0a
Ruslan Nasyrov <ruslan.nasyrov.f@gmail.com>
2024-09-25 12:34:46 +0500

    continue

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -18,6 +18,7 @@ return {
   -- customize alpha options
   {
     "goolord/alpha-nvim",
+    enabled = false,
     opts = function(_, opts)
       -- customize the dashboard header
       opts.section.header.val = {
